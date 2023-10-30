805bb40396ed8a611dcf5e96718e562d0176ee24
myeonghyun-7011 <dhvlftmd7013@naver.com>
2023-10-30 23:29:35 +0900

    GetMapping ,PostMapping ReqestParam 차이]

--- src/main/java/com/exam/sbb/Controller/MainController.java
+++ src/main/java/com/exam/sbb/Controller/MainController.java
@@ -1,8 +1,7 @@
 package com.exam.sbb.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller //스프링부트로 사용하기 위해 필수 작성
 public class MainController {
@@ -16,6 +15,36 @@ public class MainController {
     // 문자열 이기 때문에 String 사용.
 
     System.out.println("첫 시작");
-    return "안녕하세요."; // 화면에 출력
+    return "안녕하세요. asdfsaf"; // 화면에 출력
+  }
+
+  @GetMapping("/page1")
+  @ResponseBody
+  public String showGet() {
+    return """
+        <form method="POST" action="/page2"/>
+           <input type="number" name="age" placeholder="나이 입력" />
+           <input type="submit" value="page2로 POST 방식으로 이동" />
+         </form>
+         """;
+  }
+
+  @PostMapping("/page2")
+  @ResponseBody
+  public String showPage2Post(@RequestParam(defaultValue = "0") int age) {
+    return """
+        <h1>입력된 나이 : %d</h1>
+        <h1>안녕하세요. POST 방식으로 오신것을 환영합니다.</h1>
+        """.formatted(age);
+  }
+
+  @GetMapping("/page2")
+  @ResponseBody
+  public String showPost(@RequestParam(defaultValue = "0") int age) {
+    return """
+         <h1>입력된 나이 : %d</h1>
+        <h1>안녕하세요. GET 방식으로 오신것을 환영합니다.</h1>
+          
+        """.formatted(age);
   }
 }
