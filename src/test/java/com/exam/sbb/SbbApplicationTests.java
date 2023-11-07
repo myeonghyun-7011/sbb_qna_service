daaf348c6b8b2cfa5266f1523911cc304d735e39
myeonghyun-7011 <dhvlftmd7013@naver.com>
2023-11-07 17:36:49 +0900

    테스트 케이스 추가

--- src/test/java/com/exam/sbb/SbbApplicationTests.java
+++ src/test/java/com/exam/sbb/SbbApplicationTests.java
@@ -19,13 +19,19 @@ class SbbApplicationTests {
 		q1.setSubject("sbb가 무엇인가요?");
 		q1.setContent("sbb에 대해서 알고 싶습니다.");
 		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);  // 첫번째 질문 저장
+		this.questionRepository.save(q1);  // 첫번째 질문 저장 // save시점으로 id생성
+
 
 		Question q2 = new Question();
 		q2.setSubject("스프링부트 모델 질문입니다.");
 		q2.setContent("id는 자동으로 생성되나요?");
 		q2.setCreateDate(LocalDateTime.now());
 		this.questionRepository.save(q2);  // 두번째 질문 저장
+//
+//		assertThat(q1.getId()).isGreaterThan(0); // id는 최소 0 보다는 크다.
+//		assertThat(q2.getId()).isGreaterThan(q1.getId()); // 첫번째 id보다크다
+
+
 	}
 
 }
