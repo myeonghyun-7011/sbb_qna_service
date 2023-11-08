25b964c3eff78c7ef1fd7e40af9e1f9f6cf2f565
myeonghyun-7011 <dhvlftmd7013@naver.com>
2023-11-08 14:48:47 +0900

    save로 질문 수정

--- src/test/java/com/exam/sbb/SbbApplicationTests.java
+++ src/test/java/com/exam/sbb/SbbApplicationTests.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -50,7 +51,28 @@ class SbbApplicationTests {
 
   @Test
   void testJpa3() {
-    Question q = questionRepository.findBySubject("sbb가 무엇인가요?");
+    Question q = questionRepository.findByContent("sbb가 무엇인가요?");
   }
 
+  @Test
+  void testJpa4() {
+    Question q = questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해 알고 싶습니다.");
+    assertEquals(1, q.getId());
+  }
+
+  @Test
+  void testJpa5() {
+    List<Question> qList = questionRepository.findBySubjectLike("sbb%");
+    Question q = qList.get(0);
+    assertEquals("sbb가 무엇인가요?", q.getSubject());
+  }
+
+  @Test
+  void testJpa6() {
+    Optional<Question> oq = questionRepository.findById(1);
+    assertThat(oq.isPresent());
+    Question q = oq.get();
+    q.setSubject("수정된 제목");
+    questionRepository.save(q);
+  }
 }
