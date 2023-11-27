b8ecd676a77738c741ae6211c9017df951591c85
myeonghyun-7011 <dhvlftmd7013@naver.com>
2023-11-27 15:03:42 +0900

    question_LIst 템플릿 추가

--- src/main/java/com/exam/sbb/question/QuestionController.java
+++ src/main/java/com/exam/sbb/question/QuestionController.java
@@ -2,14 +2,13 @@ package com.exam.sbb.question;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class QuestionController {
 
   @GetMapping("/question/list")
-  @ResponseBody
+  // 이 자리에 @ResponseBody가 없으면 resources/templates/question_list.html 파일을 뷰로 삼는다.
   public String list() {
-    return "question list";
+    return "question_list";
   }
 }
