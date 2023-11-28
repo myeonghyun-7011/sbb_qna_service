96e9302ab97402b7ff0789ef1bd5357897b8d7de
myeonghyun-7011 <dhvlftmd7013@naver.com>
2023-11-28 10:15:11 +0900

    answercontroller 도입

---
+++ src/main/java/com/exam/sbb/answer/AnswerController.java
@@ -0,0 +1,29 @@
+package com.exam.sbb.answer;
+
+import com.exam.sbb.question.Question;
+import com.exam.sbb.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+@Controller
+public class AnswerController {
+
+  private final QuestionService questionService;
+
+  @PostMapping("/create/{id}")
+  public String createAnswer(Model model, @PathVariable int id,  String content) {
+    Question question = questionService.getQuestion(id);
+    // TODO: 답변을 저장한다.
+    
+    // 답변 등록 시작
+
+    // 답변 등록 끝
+    return String.format("redirect:/question/detail/%s", id);
+  }
+}
