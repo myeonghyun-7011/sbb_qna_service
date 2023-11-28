e56121269579300bce94c0762bf3137a0264e98b
myeonghyun-7011 <dhvlftmd7013@naver.com>
2023-11-28 21:10:49 +0900

    테스트 데이터 대량으로 만들기

--- src/test/java/com/exam/sbb/QuestionRepositoryTests.java
+++ src/test/java/com/exam/sbb/QuestionRepositoryTests.java
@@ -6,19 +6,25 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.IntStream;
+
 import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class QuestionRepositoryTests {
   @Autowired
   private QuestionRepository questionRepository;
   private static int lastSampleDataId;
+
   @BeforeEach
   void beforeEach() {
     clearData();
     createSampleData();
   }
+
   public static int createSampleData(QuestionRepository questionRepository) {
     Question q1 = new Question();
     q1.setSubject("sbb가 무엇인가요?");
@@ -33,6 +39,7 @@ class QuestionRepositoryTests {
     questionRepository.save(q2);
     return q2.getId();
   }
+
   private void createSampleData() {
 
     lastSampleDataId = createSampleData(questionRepository);
@@ -44,8 +51,10 @@ class QuestionRepositoryTests {
   }
 
   private void clearData() {
+
     clearData(questionRepository);
   }
+
   @Test
   void 저장() {
     Question q1 = new Question();
@@ -65,6 +74,7 @@ class QuestionRepositoryTests {
     assertThat(q1.getId()).isEqualTo(lastSampleDataId + 1);
     assertThat(q2.getId()).isEqualTo(lastSampleDataId + 2);
   }
+
   @Test
   void 삭제() {
     assertThat(questionRepository.count()).isEqualTo(lastSampleDataId);
@@ -73,6 +83,7 @@ class QuestionRepositoryTests {
 
     assertThat(questionRepository.count()).isEqualTo(lastSampleDataId - 1);
   }
+
   @Test
   void 수정() {
     Question q = questionRepository.findById(1).get();
@@ -81,6 +92,7 @@ class QuestionRepositoryTests {
     q = questionRepository.findById(1).get();
     assertThat(q.getSubject()).isEqualTo("수정된 제목");
   }
+
   @Test
   void findAll() {
     List<Question> all = questionRepository.findAll();
@@ -88,20 +100,42 @@ class QuestionRepositoryTests {
     Question q = all.get(0);
     assertThat(q.getSubject()).isEqualTo("sbb가 무엇인가요?");
   }
+
   @Test
   void findBySubject() {
     Question q = questionRepository.findBySubject("sbb가 무엇인가요?");
     assertThat(q.getId()).isEqualTo(1);
   }
+
   @Test
   void findBySubjectAndContent() {
     Question q = questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
     assertThat(q.getId()).isEqualTo(1);
   }
+
   @Test
   void findBySubjectLike() {
     List<Question> qList = questionRepository.findBySubjectLike("sbb%");
     Question q = qList.get(0);
     assertThat(q.getSubject()).isEqualTo("sbb가 무엇인가요?");
   }
+
+  @Test
+  void createManySampleData() {
+// 대량의 테스트 데이터 필요한 경우에만 true 변경
+    boolean run = false;
+
+    if(run == false) return;
+
+    IntStream.rangeClosed(3,300).forEach(id -> {
+      Question q =new Question();
+      q.setSubject("%d번 질문".formatted(id));
+      q.setContent("%d번 질문의 내용".formatted(id));
+      q.setCreateDate(LocalDateTime.now());
+      questionRepository.save(q);
+    });
+
+
+  }
+
 }
