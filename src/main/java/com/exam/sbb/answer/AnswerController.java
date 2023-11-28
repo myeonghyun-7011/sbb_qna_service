9b1792ead64680850c2d351963b99b3e867e786c
myeonghyun-7011 <dhvlftmd7013@naver.com>
2023-11-28 10:22:01 +0900

    답변 저장 처리

--- src/main/java/com/exam/sbb/answer/AnswerController.java
+++ src/main/java/com/exam/sbb/answer/AnswerController.java
@@ -15,14 +15,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AnswerController {
 
   private final QuestionService questionService;
+  private final  AnswerService answerService;
 
   @PostMapping("/create/{id}")
   public String createAnswer(Model model, @PathVariable int id,  String content) {
     Question question = questionService.getQuestion(id);
     // TODO: 답변을 저장한다.
-    
-    // 답변 등록 시작
 
+    // 답변 등록 시작
+    answerService.create(question, content);
     // 답변 등록 끝
     return String.format("redirect:/question/detail/%s", id);
   }
